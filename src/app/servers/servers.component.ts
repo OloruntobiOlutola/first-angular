2d795b96c7c7c7d8951d5dd5e53dec417a5f3d46
Olutola Oloruntobi Paul <oloruntobiolutola@gmail.com>
2023-04-05 18:22:00 +0100

    initial

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowClick = false;
+  allowCreateServer = 'This is disabled for now';
+  serverName = '';
+  showServer = false;
 
+  constructor() {
+    setTimeout(() => {
+      this.allowClick = true;
+    }, 2000);
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onCreateServer() {
+    this.showServer = true;
+    this.allowCreateServer =
+      'Server with name ' + this.serverName + ' has been created ';
+  }
 }
